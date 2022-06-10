37cbc8a5a655f1a17c87c52dd02b6974f35aab37
Facundo Rusman <rusmanfacundo@gmail.com>
2022-06-10 15:11:00 -0300

    Login page avances

--- src/app/components/auth/login/login.page.ts
+++ src/app/components/auth/login/login.page.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  showPassword = false;
+  passwordToggleIcon = 'eye';
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  togglePassword(): void{
+    this.showPassword = !this.showPassword;
+    if (this.passwordToggleIcon === 'eye') {
+      this.passwordToggleIcon = 'eye-off';
+    } else {
+      this.passwordToggleIcon = 'eye';
+    }
   }
 
 }
