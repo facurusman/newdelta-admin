35661cf026590d436a917a3cb670797ec9f742a8
lucasrusman <rusmanlucas@gmail.com>
2022-06-14 14:31:15 -0300

    spinner bookings added

--- src/app/components/reservas/reservas.page.ts
+++ src/app/components/reservas/reservas.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { LoadingController } from '@ionic/angular';
 import { Subscription } from 'rxjs';
 import { Reserva } from 'src/app/models/reserva';
 import { BookingsService } from 'src/app/services/bookings.service';
@@ -11,24 +12,29 @@ import { BookingsService } from 'src/app/services/bookings.service';
 })
 export class ReservasPage implements OnInit {
   reservas: Reserva[];
+  isLoading = false;
   constructor(
     private router: Router,
-    private bookingService: BookingsService
+    private bookingService: BookingsService,
+    private loadingCtrl: LoadingController
   ) {}
 
   ngOnInit() {
-    this.allBookings()
+    this.allBookings();
   }
   allBookings() {
     return this.bookingService.getBookings().subscribe((bookings) => {
       this.reservas = bookings as Reserva[];
       console.log(this.reservas);
-      
-    })
+    });
   }
   goToBookingAceptPage(id: number) {
-    return this.bookingService.aceptBooking(id).subscribe(() => {
-      this.router.navigateByUrl('booking-acept');
-    })
+    this.loadingCtrl.create({ message: 'Aceptando...' }).then((loadingEl) => {
+      loadingEl.present();
+      this.bookingService.aceptBooking(id).subscribe(() => {
+        loadingEl.dismiss();
+        this.router.navigateByUrl('booking-acept');
+      });
+    });
   }
 }
